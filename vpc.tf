497e21fbc7394e0deef01baa608b19d0b6726f96
Michael B. Klein <mbklein@gmail.com>
2023-03-15 17:45:22 +0000

    Eliminate unnecessary and pricey NAT gateway

--- vpc.tf
+++ vpc.tf
@@ -11,9 +11,8 @@ module "vpc" {
   
   enable_dns_hostnames = true
   enable_dns_support   = true
-  enable_nat_gateway   = true
+  enable_nat_gateway   = false
   enable_vpn_gateway   = false
-  single_nat_gateway   = true
 
   create_database_subnet_group = false
 
